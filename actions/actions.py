456035167cece9b033b702610b509afe4ec8d92b
ThangDuong59 <60088921+ThangDuong59@users.noreply.github.com>
2020-11-28 19:35:28 +0700

    import FormValidationAction lib

--- actions/actions.py
+++ actions/actions.py
@@ -1,8 +1,9 @@
 from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.forms import FormAction
+from rasa_sdk.types import DomainDict
 
 class AnswerCppDefineQuestion(Action):
 
